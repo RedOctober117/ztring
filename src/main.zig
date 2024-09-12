a61e80add0f28de9004580c8a4ca3aa6f181d8e0
RedOctober117 <dbcahilly3@gmail.com>
2024-09-11 22:44:05 -0500

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-// const String = @import("root.zig").String;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -8,15 +7,9 @@ pub fn main() !void {
     var string = try allocator.alloc(u8, 10);
     errdefer allocator.free(string);
 
-    // @memcpy(string[0..], 0);
-
     const str = "test";
 
     @memcpy(string[0..str.len], str);
 
     std.debug.print("{s}\n", .{string});
-
-    // const test_string = String.from(allocator, "test");
-    // std.debug.print("{}\n}", .{test_string.get()});
-    // _ = String.from("test");
 }
