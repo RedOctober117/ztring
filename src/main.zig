ca84112ef71997aafaac22ea75fd96094804e5a4
RedOctober117 <dbcahilly3@gmail.com>
2024-09-12 15:27:16 -0500

    working

--- src/main.zig
+++ src/main.zig
@@ -4,22 +4,13 @@ const String = @import("root.zig").String;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+}
 
-    var string = try allocator.alloc(u8, 10);
-    errdefer allocator.free(string);
+test "string init" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    const str = "test";
+    const new_str = try String.init(allocator);
 
-    @memcpy(string[0..str.len], str);
-
-    std.debug.print("{s}\n", .{string});
-
-    const new_string = String.init(allocator);
-    defer new_string.deinit();
-
-    std.debug.print("Empty string: {s}\n", .{new_string.get()});
-
-    new_string.put("test");
-
-    std.debug.print("Full string: {s}\n", .{new_string.get()});
+    try std.testing.expect(std.mem.eql(u8, new_str.get(), ""));
 }
--- src/root.zig
+++ src/root.zig
@@ -2,65 +2,85 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 // const FixedBufferAllocator = std.heap.FixedBufferAllocator;
 
+// fn name(self: Self) returns a copy of the value. if the value contains a
+// pointer in a field, a copy of that pointer is made and can still be
+// dereferenced. non-pointer values are not mutable unless the parameter is
+// (self: *Self).
+
 const buffer_size = 10;
 
 pub const String = struct {
+    //ptr, ptr, value
     allocator: Allocator,
     string_ptr: []u8,
     length: usize,
 
-    fn capacity(self: String) usize {
+    pub fn capacity(self: String) usize {
         return self.string_ptr.len;
     }
 
-    pub fn init(allocator: Allocator) String {
+    pub fn init(allocator: Allocator) !String {
         return String{
             .allocator = allocator,
-            .string_ptr = allocator.alloc(u8, buffer_size) catch unreachable,
+            .string_ptr = try allocator.alloc(u8, buffer_size),
             .length = 0,
         };
-
-        // var i = undefined;
-
-        // for (0..new_str.string_ptr.len) |i| {
-        //     new_str.string_ptr[i] = 0;
-        // }
-
-        // return new_str;
     }
 
     pub fn deinit(self: String) void {
         self.allocator.free(self.string_ptr);
     }
 
-    pub fn from(allocator: Allocator, string: []const u8) String {
-        var new_string = String.init(allocator);
+    pub fn from(allocator: Allocator, string: []const u8) !String {
+        var new_string = try String.init(allocator);
         if (new_string.capacity() < string.len) {
             new_string.resize_with_len(string.len);
         }
         new_string.put(string);
 
-        return string;
-        // @memcpy(new_string.string_ptr.*, string);
+        return new_string;
     }
 
     pub fn get(self: String) []u8 {
         return self.string_ptr[0..self.length];
     }
 
-    pub fn put(self: String, string: []const u8) void {
+    pub fn put(self: *String, string: []const u8) void {
         if (string.len > self.capacity()) {
-            self.resize_with_len(string.len);
+            _ = &self.resize_with_len(string.len);
         }
 
         @memcpy(self.string_ptr[0..string.len], string);
+        self.length = string.len;
     }
 
-    fn resize(self: String) !void {
-        _ = self.allocator.resize(self.string_ptr, self.length + buffer_size);
+    fn resize(self: *String) void {
+        const new_len = self.length + buffer_size;
+        _ = self.allocator.resize(self.string_ptr, new_len);
     }
 
-    fn resize_with_len(self: String, length: usize) void {
+    fn resize_with_len(self: *String, length: usize) void {
+        std.debug.print("inc len {d}", .{length});
         _ = self.allocator.resize(self.string_ptr, length);
     }
 };
+
+// const print = @import("std").debug.print;
+
+// const Duck = struct { Hp: i8, const Self = @This(); fn sleep(self: *Self) void { self.Hp += 1; } };
+
+// const RubberDuck = struct { Hp: i8, const Self = @This(); fn sleep(self: *Self) void { self.Hp -= 1; } };
+
+// fn Rest(t: anytype) void { t.sleep(); }
+
+// pub fn main() void { var d = Duck{ .Hp = 0, };
+
+// var rd = RubberDuck{
+//     .Hp = 0,
+// };
+
+// Rest(&d);
+// Rest(&rd);
+// print("{d},{d}\n", .{ d.Hp, rd.Hp });
+
+// }
